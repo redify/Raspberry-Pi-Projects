c4d13ca707ed9d985a04d0739761e304cab86784
Greg <gsjohnson667@gmail.com>
2016-02-05 00:23:04 +0000

    got collection script working

--- weather-station/collect_data.py
+++ weather-station/collect_data.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python
 
-import pymysql
+import _mysql
 from bme280_lib2 import *
-
 
 sensor = BME280(mode=BME280_OSAMPLE_16)
 
-db = pymysql.connect(host='localhost', port=3306, user='root', passwd='Eing9Cah&', db='weather')
-cur = db.cursor()
+db = _mysql.connect(host='localhost', port=3306, user='root', passwd='Eing9Cah&', db='weather')
 
 degrees = sensor.read_temperature()
 pascals = sensor.read_pressure()
 hectopascals = pascals / 100
 humidity = sensor.read_humidity()
 
-sql_command = "INSERT INTO base_obs VALUES(NOW(), {0:0.2f}, {1:0.2f}, {2:0.2f});".format(degrees, humidity, hectopascals)
-#print (sql_command)
-#exit()
+sql_command = "INSERT INTO base_obs VALUES(NOW(), {0:0.3f}, {1:0.3f}, {2:0.3f});".format(degrees, humidity, hectopascals)
 
-cur.execute(sql_command)
+db.query(sql_command)
 
-cur.close()
 db.close()
